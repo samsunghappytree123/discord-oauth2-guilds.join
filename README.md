509a178812672bc7b89633a0242aa59111157c99
samsunghappytree123 <samsunghappytree123@naver.com>
2021-07-21 16:52:16 +0900

    feat: all files upload.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # discord-oauth-guilds.join
 Discord Oauth2의 "Guilds.join" Scope를 활용한 코드입니다.
+
+## １ 사용법
+1. 이 코드를 clone 또는 fork 등으로 다운로드합니다.
+2. cmd에 ``pip install -r "requirements.txt"``를 사용해 필요한 모듈을 설치합니다.
+3. [``.env.example``](.env.example) 파일을 참고하여 ``.env`` 파일을, [``config.py.example``](config.py.example)을 참고하여 ``config.py``를 만들어주세요. (자세한 사항은 [여기](#envexample과-configpyexample-내용-설명)을 확인해주세요.)
+4. [``app.py``](app.py)를 실행해주세요.
+
+---
+
+## ２ .env.example과 config.py.example 내용 설명
+
+### .env.example
+```py
+TOKEN=YOUR_TOKEN # 봇의 토큰을 입력해주세요.
+OAUTH2_CLIENT_SECRET=YOUR_SECRET_KEY # 봇의 CLIENT_SECRET_KEY를 입력해주세요.
+```
+
+### config.py.example
+```py
+GUILD_ID = 123456789 # 강제 초대할 서버의 ID를 입력하세요.
+CLIENT_ID = 123456789 # CLIENT ID (BOT ID)를 입력해주세요.
+REDIRECT_URI = 'http://localhost:5000/callback' # OAUTH 로그인 후 이동할 웹사이트의 주소를 입력하세요.
+
+# -- Sanic 호스트 설정 -- #
+HOST = '127.0.0.1' # 127.0.0.1과 같이 수정해주세요.
+PORT = 5000 # 포트를 지정해주세요.
+```
---
+++ app.py
@@ -0,0 +1,74 @@
+import aiohttp, os, asyncio, dotenv, config
+from sanic import Sanic, response, request
+from sanic.response import json, text, html, redirect
+from requests_oauthlib import OAuth2Session
+
+dotenv.load_dotenv(verbose=True)
+
+app = Sanic(__name__)
+app.config["SECRET_KEY"] = os.getenv("OAUTH2_CLIENT_SECRET")
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+if "http://" in os.getenv("OAUTH2_CLIENT_SECRET"):
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
+
+AUTHORIZATION_BASE_URL = "https://discord.com/api/oauth2/authorize"
+TOKEN_URL = "https://discord.com/api/oauth2/token"
+
+def token_updater(token):
+    request.cookies["oauth2_token"] = token
+
+
+def make_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=config.CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=config.REDIRECT_URL,
+        auto_refresh_kwargs={
+            "client_id": config.CLIENT_ID,
+            "client_secret": app.config["SECRET_KEY"],
+        },
+        auto_refresh_url=TOKEN_URL,
+        token_updater=token_updater,
+    )
+
+@app.route("/login")
+async def login(request):
+    scope = request.args.get("scope", "identify email guilds guilds.join")
+    discordoauth = make_session(scope=scope.split(" "))
+    authorization_url, state = discordoauth.authorization_url(AUTHORIZATION_BASE_URL)
+    return redirect(authorization_url)
+
+@app.route("/error")
+async def error(request):
+    return text("어라...? 알 수 없는 오류가 발생했어요.")
+
+@app.route("/callback")
+async def callback(request):
+    code = request.args.get('code')
+    if request.args.get("error"):
+        return redirect("/error")
+    discordoauth = make_session()
+    token = discordoauth.fetch_token(
+        TOKEN_URL,
+        client_secret=app.config["SECRET_KEY"],
+        authorization_response=request.url,
+    )
+    user = discordoauth.get("https://discord.com/api/users/@me").json()
+    url = f"https://discord.com/api/guilds/{config.GUILD_ID}/members/{user['id']}"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.put(url, json={'access_token' : token['access_token']}, headers={"Authorization": f"Bot {os.getenv('TOKEN')}", "Content-Type":"application/json"}) as response:
+            try:
+                data = await response.json()
+            except:
+                return 
+
+    return text("서버 강제 초대가 성공적으로 이루어졌습니다.")
+
+
+if __name__ == "__main__":
+    app.run(host=config.HOST, port=config.PORT)
